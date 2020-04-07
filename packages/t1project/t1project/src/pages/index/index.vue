d4a3d7edbbc99a86b090ab46322c13603ec037ac
王敏 <569049370@qq.com>
2020-04-07 15:59:22 +0800

    t1project using t1-2 update

--- packages/t1project/t1project/src/pages/index/index.vue
+++ packages/t1project/t1project/src/pages/index/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <div>you see ?Iam a main page and main project include using components in other file</div>
+    <cakeone></cakeone>
   </div>
 </template>
 
 <script>
-
+import cakeone from 't1-2/src/components/cakeone'
 export default {
   data () {
     return {
     }
+  },
+  components: {
+    cakeone
   },
   methods: {},
 
